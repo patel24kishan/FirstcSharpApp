78e8c4c8b6126482bc204853f31a509a6e8839e8
ghosts24 <35022263+patel24kishan@users.noreply.github.com>
2023-02-06 17:01:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FirstcSharpApp
+ 
---
+++ Shop.cs
@@ -0,0 +1,15 @@
+namespace FirstcSharpApp
+{
+    public partial class Shop : Form
+    {
+        public Shop()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
